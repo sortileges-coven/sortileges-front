6339d3c6efa363db8938090b2635b8de0cd10cb7
Clément Roig <clm.roig@gmail.com>
2022-08-10 15:19:28 +0200

    home page first try

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,20 +1,18 @@
 <template>
   <q-page>
-    <div class="bg-red h-screen h-screen">
-      <div>
-        <h1 class="cursive-font">
-          {{ $t('welcome.message') }}
-        </h1>
-        <p class="my-0 secondary-font">
-          {{ $t('welcome.headline') }}
-        </p>
-        <hr />
-        <p>{{ $t('welcome.joinCommunity') }}</p>
-        <div>
-          <q-btn>{{ $t('login') }}</q-btn>
-          <q-btn>{{ $t('register') }}</q-btn>
-          <q-btn>{{ $t('welcome.knowMore') }}</q-btn>
-        </div>
+    <div class="welcome-bg h-screen h-screen flex column flex-center">
+      <h1 class="cursive-font q-ma-none">
+        {{ $t('welcome.message') }}
+      </h1>
+      <p class="secondary-font">
+        {{ $t('welcome.headline') }}
+      </p>
+      <hr />
+      <p>{{ $t('welcome.joinCommunity') }}</p>
+      <div class="q-pa-md q-gutter-sm">
+        <q-btn>{{ $t('login') }}</q-btn>
+        <q-btn>{{ $t('register') }}</q-btn>
+        <q-btn>{{ $t('welcome.knowMore') }}</q-btn>
       </div>
     </div>
     <div class="bg-green h-screen h-screen">Section 2</div>
@@ -29,6 +27,7 @@
   background-image: url('../assets/images/witch-halloween.jpg');
   background-position: center;
   background-size: cover;
+  color: #fff;
 }
 .h-screen {
   height: 100vh;
