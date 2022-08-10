662171cc45d0f69f1117db858f2128e1099aa5eb
Clément Roig <clm.roig@gmail.com>
2022-08-10 15:05:21 +0200

    import custom fonts

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,14 +1,35 @@
 <template>
-  <q-page class="column items-center">
-    <div class="h-screen w-screen bg-red">Home Page</div>
-    <div class="h-screen w-screen bg-green">Section 2</div>
-    <div class="h-screen w-screen bg-blue">Section 3</div>
+  <q-page>
+    <div class="bg-red h-screen h-screen">
+      <div>
+        <h1 class="cursive-font">
+          {{ $t('welcome.message') }}
+        </h1>
+        <p class="my-0 secondary-font">
+          {{ $t('welcome.headline') }}
+        </p>
+        <hr />
+        <p>{{ $t('welcome.joinCommunity') }}</p>
+        <div>
+          <q-btn>{{ $t('login') }}</q-btn>
+          <q-btn>{{ $t('register') }}</q-btn>
+          <q-btn>{{ $t('welcome.knowMore') }}</q-btn>
+        </div>
+      </div>
+    </div>
+    <div class="bg-green h-screen h-screen">Section 2</div>
+    <div class="bg-blue h-screen h-screen">Section 3</div>
   </q-page>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped>
+.welcome-bg {
+  background-image: url('../assets/images/witch-halloween.jpg');
+  background-position: center;
+  background-size: cover;
+}
 .h-screen {
   height: 100vh;
 }
