796330c7d05e2da3bc2a67fedee0ef3c67234472
Clement <clm.roig@gmail.com>
2022-08-16 15:12:47 +0200

    chore(env): add env file

--- quasar.config.js
+++ quasar.config.js
@@ -48,6 +48,10 @@ module.exports = configure(function (/* ctx */) {
 
     // Full list of options: https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#build
     build: {
+      alias: {
+        '@': path.join(__dirname, './src'),
+      },
+      env: require('dotenv').config().parsed,
       target: {
         browser: ['es2019', 'edge88', 'firefox78', 'chrome87', 'safari13.1'],
         node: 'node16',
